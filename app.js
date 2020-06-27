e2bbf27c630f603aa4af365d2e81a8d85107facf
Bluesmoke <43018850+hibenca@users.noreply.github.com>
2020-06-27 09:38:48 -0500

    Version 1.0

--- app.js
+++ app.js
@@ -1,4 +1,40 @@
-document.getElementById('start').onclick = function startStopwatch() {
-    console.log("TEst!");
-    
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+let domSeconds = document.getElementById('seconds');
+let domMinutes = document.getElementById('minutes');
+let domHours = document.getElementById('hours');
+
+function timer() {
+    setTimeout(add, 1000);
+};
+
+function add() {
+    if (seconds >= 60) {
+        seconds = 0;
+        domMinutes.innerHTML = minutes++
+        timer()
+    } else if (minutes >= 60) {
+        minutes = 0;
+        domHours.innerHTML = hours++
+        timer()
+    } else {
+        domSeconds.innerHTML = seconds++
+        timer();
+    }
 }
+
+function start() {
+    add()
+}
+
+
+function stop() {
+    console.log("Stop");
+
+}
+
+function reset() {
+    clearTimeout();
+};
