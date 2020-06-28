c1748a4239ac9e183a28916ebae70478bd3ec8c9
Bluesmoke <43018850+hibenca@users.noreply.github.com>
2020-06-28 11:06:13 -0500

    Stopwatch version 2.0

--- app.js
+++ app.js
@@ -1,40 +1,76 @@
+//Define vars to hold time values
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-let domSeconds = document.getElementById('seconds');
-let domMinutes = document.getElementById('minutes');
-let domHours = document.getElementById('hours');
+//Define vars to hold "display" value
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
 
-function timer() {
-    setTimeout(add, 1000);
-};
+//Define var to hold setInterval() function
+let t = null;
 
-function add() {
+//Define var to hold stopwatch status
+let status = "stopped";
+
+//Define time DOM ID
+let time = document.getElementById('time');
+
+
+//Stopwatch functional logic
+function stopWatch() {
+
+    seconds++
+
     if (seconds >= 60) {
         seconds = 0;
-        domMinutes.innerHTML = minutes++
-        timer()
-    } else if (minutes >= 60) {
-        minutes = 0;
-        domHours.innerHTML = hours++
-        timer()
-    } else {
-        domSeconds.innerHTML = seconds++
-        timer();
+        minutes++;
+
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++
+
+        }
+
     }
+
+    //if seconds/minutes/hours are only one digit, add a leading 0 to the value
+    if (seconds < 10) {
+        displaySeconds = "0" + seconds.toString();
+    } else {
+        displaySeconds = seconds;
+    }
+    if (minutes < 10) {
+        displayMinutes = "0" + minutes.toString();
+    } else {
+        displayMinutes = minutes;
+    }
+    if (hours < 10) {
+        displayHours = "0" + hours.toString();
+    } else {
+        displayHours = hours;
+    }
+
+    //Display updated time values to user
+    time.innerHTML = displaySeconds + ":" + displayMinutes + ":" + displayHours;
 }
 
-function start() {
-    add()
-}
+function Start() {
+    status = "started";
+    t = window.setInterval(stopWatch, 1000);
+};
+
+function Stop() {
+    status = "stopped";
+    window.clearInterval(t);
+};
 
 
-function stop() {
-    console.log("Stop");
-
-}
-
-function reset() {
-    clearTimeout();
+function Reset() {
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    window.clearInterval(t);
+    time.innerHTML = "00:00:00"
 };
